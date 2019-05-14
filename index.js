a470a14a96ad40d2a113328111ce227224967dce
anupant01 <pantaanupa@gmail.com>
2019-05-14 21:12:14 +0545

    npm intialized again due to some error

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ var express = require('express');
 var app = new express();
 
 
-app.listen(5002);
+app.post('/v1/users',
+	function(req,res,next){
+
+		
+})
+
+
+app.listen(process.env.PORT);
 console.log("server is running");
