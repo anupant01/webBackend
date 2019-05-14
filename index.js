fa76290810d0459998159e1e294390075a6b245b
anupant01 <pantaanupa@gmail.com>
2019-05-14 21:45:32 +0545

    body-parser intialized

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ var express = require('express');
 var app = new express();
 
 
+app.use(function (req,res,next) {
+
+	res.setHeader('Access-Control-Allow-Origin','*');
+	res.setHeader('Access-Control-Allow-Headers','content-type')
+	next();
+	
+})
+
+
 app.post('/v1/users',
 	function(req,res,next){
-
+		
 		
 })
 
